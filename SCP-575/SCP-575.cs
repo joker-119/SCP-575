aff4bf12709f7df5a5059c5289ea99283eda19d1
galaxy119 <amathor929@gmail.com>
2019-04-04 18:01:28 -0700

    Update to 3.4.0

--- SCP-575/SCP-575.cs
+++ SCP-575/SCP-575.cs
@@ -13,9 +13,9 @@ namespace SCP575
 		name = "SCP-575",
 		description = "Adds light blackout command + timed events",
 		id = "joker.SCP575",
-		version = "2.4.0",
+		version = "2.5.0",
 		SmodMajor = 3,
-		SmodMinor = 3,
+		SmodMinor = 4,
 		SmodRevision = 0
 	)]
 
@@ -37,26 +37,26 @@ namespace SCP575
 
 		public override void Register()
 		{
-			this.AddConfig(new ConfigSetting("575_ranks", new string[] { }, SettingType.LIST, true, "The list of ranks able to use the LO commands."));
-			this.AddConfig(new ConfigSetting("575_timed", true, SettingType.BOOL, true, "If timed events should be active."));
-			this.AddConfig(new ConfigSetting("575_delay", 300f, SettingType.FLOAT, true, "The amount of seconds before the first timed blackout occurs."));
-			this.AddConfig(new ConfigSetting("575_duration", 90f, SettingType.FLOAT, true, "The amount of time that Lightout events last."));
-			this.AddConfig(new ConfigSetting("575_wait", 180f, SettingType.FLOAT, true, "The amoutn of time to wait between subsequent timed events."));
-			this.AddConfig(new ConfigSetting("575_announce", true, SettingType.BOOL, true, "If CASSIE should announce events."));
-			this.AddConfig(new ConfigSetting("575_toggle_lcz", true, SettingType.BOOL, true, "If toggled events should affect LCZ."));
-			this.AddConfig(new ConfigSetting("575_timed_lcz", true, SettingType.BOOL, true, "If timed events should affect LCZ."));
-			this.AddConfig(new ConfigSetting("575_toggle_tesla", true, SettingType.BOOL, true, "If teslas should be disabled during toggled events."));
-			this.AddConfig(new ConfigSetting("575_timed_tesla", true, SettingType.BOOL, true, "If teslas should be disabled during timed events."));
-			this.AddConfig(new ConfigSetting("575_keter_damage", 10, SettingType.NUMERIC, true, "How much damage per 5 seconds people in affected areas take."));
-			this.AddConfig(new ConfigSetting("575_keter", true, SettingType.BOOL, true, "If SCP-575 should make use of it's Keter status."));
-			this.AddConfig(new ConfigSetting("575_keter_toggle", false, SettingType.BOOL, true, "If SCP-575 keter should be enabled for toggled events."));
-			this.AddConfig(new ConfigSetting("575_keter_kill", false, SettingType.BOOL, true, "If SCP-575's keter event should kill players instead of damage them."));
-			this.AddConfig(new ConfigSetting("575_keter_kill_num", 1, SettingType.NUMERIC, true, "The number of players killed during timed Keter events.));"));
-			this.AddConfig(new ConfigSetting("575_random_events", false, SettingType.BOOL, true, "If 575 events should be randomized."));
-			this.AddConfig(new ConfigSetting("575_random_min", 60, SettingType.NUMERIC, true, "The minimum amount of time between random events."));
-			this.AddConfig(new ConfigSetting("575_random_max", 300, SettingType.NUMERIC, true, "The maximum wait time between random events."));
-			this.AddConfig(new ConfigSetting("575_random_dur_min", 11, SettingType.NUMERIC, true, "The minimum amount of time a random event should last."));
-			this.AddConfig(new ConfigSetting("575_random_dur_max", 90, SettingType.NUMERIC, true, "The maximum amount of time a random event should last."));
+			this.AddConfig(new ConfigSetting("575_ranks", new string[] { }, true, "The list of ranks able to use the LO commands."));
+			this.AddConfig(new ConfigSetting("575_timed", true, true, "If timed events should be active."));
+			this.AddConfig(new ConfigSetting("575_delay", 300f, true, "The amount of seconds before the first timed blackout occurs."));
+			this.AddConfig(new ConfigSetting("575_duration", 90f, true, "The amount of time that Lightout events last."));
+			this.AddConfig(new ConfigSetting("575_wait", 180f, true, "The amoutn of time to wait between subsequent timed events."));
+			this.AddConfig(new ConfigSetting("575_announce", true, true, "If CASSIE should announce events."));
+			this.AddConfig(new ConfigSetting("575_toggle_lcz", true, true, "If toggled events should affect LCZ."));
+			this.AddConfig(new ConfigSetting("575_timed_lcz", true, true, "If timed events should affect LCZ."));
+			this.AddConfig(new ConfigSetting("575_toggle_tesla", true, true, "If teslas should be disabled during toggled events."));
+			this.AddConfig(new ConfigSetting("575_timed_tesla", true, true, "If teslas should be disabled during timed events."));
+			this.AddConfig(new ConfigSetting("575_keter_damage", 10, true, "How much damage per 5 seconds people in affected areas take."));
+			this.AddConfig(new ConfigSetting("575_keter", true, true, "If SCP-575 should make use of it's Keter status."));
+			this.AddConfig(new ConfigSetting("575_keter_toggle", false, true, "If SCP-575 keter should be enabled for toggled events."));
+			this.AddConfig(new ConfigSetting("575_keter_kill", false, true, "If SCP-575's keter event should kill players instead of damage them."));
+			this.AddConfig(new ConfigSetting("575_keter_kill_num", 1, true, "The number of players killed during timed Keter events.));"));
+			this.AddConfig(new ConfigSetting("575_random_events", false, true, "If 575 events should be randomized."));
+			this.AddConfig(new ConfigSetting("575_random_min", 60, true, "The minimum amount of time between random events."));
+			this.AddConfig(new ConfigSetting("575_random_max", 300, true, "The maximum wait time between random events."));
+			this.AddConfig(new ConfigSetting("575_random_dur_min", 11, true, "The minimum amount of time a random event should last."));
+			this.AddConfig(new ConfigSetting("575_random_dur_max", 90, true, "The maximum amount of time a random event should last."));
 
 			this.AddEventHandlers(new EventsHandler(this), Priority.Normal);
 
